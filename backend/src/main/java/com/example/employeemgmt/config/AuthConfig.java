eb604e5f8f1548fc537f680645ebec3522ac4a50
dalwadi.dilip@gmail.com <dalwadi.dilip@gmail.com>
2024-04-11 13:46:47 +0530

    Fixed CORS error

--- backend/src/main/java/com/example/employeemgmt/config/AuthConfig.java
+++ backend/src/main/java/com/example/employeemgmt/config/AuthConfig.java
@@ -1,15 +1,21 @@
 package com.example.employeemgmt.config;
 
 import com.example.employeemgmt.utils.CookieUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.ErrorResponse;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.file.AccessDeniedException;
 import java.util.List;
 
 @Configuration
@@ -25,9 +31,16 @@ public class AuthConfig extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         } else if (!CookieUtils.isCookieExist(request, "user")) {
-            response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "User is unauthorized");
+
+            ErrorResponse re = ErrorResponse.create(new AccessDeniedException("Access Denied"), HttpStatus.UNAUTHORIZED, "No Cookie");
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            OutputStream responseStream = response.getOutputStream();
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.writeValue(responseStream, re);
+            responseStream.flush();
             filterChain.doFilter(request, response);
-            return; //   <--- important line
+            return;
         }
         filterChain.doFilter(request, response);
     }
